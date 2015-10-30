f8e9a4c139dca101112cc232513ae8c64ba82f45
Sahil Dhanju <sahildhanju1@gmail.com>
2015-10-30 17:42:11 -0500

    inital commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var request = require('request')
+var cheerio = require('cheerio');
+
+app = express();
+
+app.use('/static', express.static(__dirname + '/public'));
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res)
+{
+	res.render('static/index.html');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
