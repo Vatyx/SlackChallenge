c0b4d5989b4cbd7de2ed04c767b585ddcd9bd16e
Sahil Dhanju <sahildhanju1@gmail.com>
2015-11-03 00:16:40 -0600

    Created counter for different tags

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,8 @@
 console.log("hi there");
 
+var source = "";
+var tagsCount = {};
+
 $("#field").keyup(function(event){
     if(event.keyCode === 13){
         $("#submit").click();
@@ -8,14 +11,52 @@ $("#field").keyup(function(event){
 
 function getSource()
 {
-	$.ajax({url: "/query", data: {url: "afsd"}, success: function(data, status)
+	$.ajax({url: "/query", data: {url: "http://www.reddit.com"}, success: function(data, status)
 	{
 		console.log(data.body);
-
-
+		source = data.body;
+		$('#codehere').text(source);
+		computeTags();
 	}, 
 	error: function()
 	{
 		console.log("Oh shucks an error");
 	}});
+}
+
+function computeTags()
+{	
+	var foundTag = false;
+	var currentTag = "";
+	for(var i = 0; i < source.length; i++)
+	{
+		if(source[i] === "<")
+		{
+			if(source[i+1] !== "/" || source[i+1] !== "!")
+			{
+				foundTag = true;
+			}
+			continue;
+		}
+		else if(foundTag)
+		{
+			if(source[i] === ">" || source[i] === " ")
+			{
+				foundTag = false;
+				if(tagsCount[currentTag] === undefined)
+				{
+					tagsCount[currentTag] = 1;
+				}
+				else
+				{
+					tagsCount[currentTag]++;
+				}
+				currentTag = "";
+			}
+			else
+			{
+				currentTag = currentTag.concat(source[i]);
+			}
+		}
+	}
 }
