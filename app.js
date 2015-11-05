4c8a6a8053f2cbe1b2345f7be8462f7a4a97b21c
Sahil Dhanju <sahildhanju1@gmail.com>
2015-11-05 02:36:37 -0600

    added a bunch of comments

--- app.js
+++ app.js
@@ -6,70 +6,95 @@ var html = require('html');
 
 app = express();
 
+//listening on this port because of heroku
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));	//where static files will be
 app.set('views', __dirname + '/views')
-app.set('view engine', 'jade');
+app.set('view engine', 'jade');	//using Jade
 
+//...........
+//Routing
+//...........
+
+//Base route
 app.get('/', function(req, res)
 {
 	console.log("In base");
-	res.render('index.jade');
+
+	res.render('index.jade');	//return base file
 });
 
+//Query route
+//Query for specific url that user inputs.
+//If successful then return html and object containing tags and their count
+//If not then send a 500
 app.get('/query', function(req, res)
 {
 	console.log("In query");
 	
-	var url = req.query.url;
+	var url = req.query.url;	//get URL
+	//make sure URL is in the correct format (add http:// if it isnt't here)
 	if (!url.match(/^[a-zA-Z]+:\/\//))
 	{
 	    url = 'http://' + url;
 	}
 
-	var payload = "";
+	//payload to be sent
+	var payload = {};
+	//make the request
 	request(url, function (error, response, body) {
+		//if request recieved a response
 	 	if(response!=undefined)
 	 	{
 	 		res.status(200);
-	 		payload = computeTagsCount(body);
-	 		body = html.prettyPrint(body, {indent_size: 4});
-	 		res.json({"body": body, "count": payload});
+
+	 		payload = computeTagsCount(body); //get the count of all the tags
+	 		body = html.prettyPrint(body, {indent_size: 4});	//make the response html pretty
+	 		res.json({"body": body, "count": payload});	//send 
 	 	}
+	 	//request failed
 	 	else
 	 	{
 	 		res.status(500).send();
 	 	}
 	});
-	console.log("got here");
 })
 
+//Given a string, object containing the html tags present and the
+//count of each of them will be returned
 function computeTagsCount(source)
 {	
-	var tagsCount = {};
-	var foundTag = false;
-	var currentTag = "";
+	var tagsCount = {}; //object to be returned
 
-	var alpha = /(?:<)(\w+)/g;
+	//regex for finding a html tag
+	//an HTML tag has to start with <, and then only alphanumeric characters can appear.
+	//the < character is matched but not captured
+	var alpha = /(?:<)(\w+)/g;	
 
+	//match the tag
 	var mine = alpha.exec(source);
+	//keep executing until no more tags are found
 	while(mine != null)
 	{
+		//if the tag doesn't exist in the object, add it
 		if(tagsCount[mine[1]] === undefined)
 		{
 			tagsCount[mine[1]] = 1;
 		}
+		//otherwise increment it
 		else
 		{
 			tagsCount[mine[1]]++;
 		}
+		//find the next tag
 		var mine = alpha.exec(source);
 	}
 
 	return tagsCount;
 }
 
+//listen to the port
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,31 +1,41 @@
-var source = "";
-var tagsCount = {};
+var source = "";	//html source code
+var tagsCount = {};	//object with tags and their counts
 
+//if enter is pressed while focused on the input field, click the button
 $("#field").keyup(function(event){
     if(event.keyCode === 13){
         $("#submit").click();
     }
 });
 
+//make the query for the url the user inputed and populate the DOM
+//with the source code and buttons to highlight the code
 function getSource()
 {
-
+	//make the query
 	$.ajax({url: "/query", data: {url: $("#field").val()}, success: function(data, status)
 	{
+		//successful request
+
+		//if the containers haven't been initally opened, open them
 		if(!$('.box').hasClass('open'))
 		{
-			source = data.body;
-			$('#precode').text(source);
-			tagsCount = data.count;
-			addButtons();
+			source = data.body;			//get the source code
+			$('#precode').text(source);	//insert the source code
+			tagsCount = data.count;		//get the tagsCount object
+			addButtons();				//insert buttons into the container
+			//scroll down for the user
 			$('body,html').animate({scrollTop: $('#buttonContainer').position().top +50}, 500);
 
+			//show the containers
 			$('.header').addClass('moveup');
 			$('.box').addClass('open');
 		}
 		else
-		{
+		{	
+			//move the containers off the screen
 			$('.box').addClass('animated bounceOutRight');
+			//get everything after 500 milliseconds
 			setTimeout(function()
 			{	
 				source = data.body;
@@ -33,32 +43,40 @@ function getSource()
 				tagsCount = data.count;
 				addButtons();
 				
-				$('body,html').animate({scrollTop: $('#buttonContainer').position().top +50}, 500);
+				//bring the containers back
 				$('.box').removeClass('animated bounceOutRight');
 				$('.box').addClass('animated bounceInLeft');
+				//scroll down for the user
+				$('body,html').animate({scrollTop: $('#buttonContainer').position().top +50}, 500);
 			}, 500);
 		}
-	}, 
+	},
+	//unsucessful request
 	error: function()
 	{
 		console.log("Oh shucks an error");
 
+		//display the tooltip for 1600 milliseconds
 		$('.tooltip').addClass('open');
 		setTimeout(function()
 		{
 			$('.tooltip').removeClass('open');
-		}, 1500);
+		}, 1600);
 	}});
 }
 
+//add buttons to the #buttonContainer element
 function addButtons()
 {
+	//get the DOM element
 	$buttonContainer = $('#buttonContainer');
-	$buttonContainer.empty();
+	$buttonContainer.empty(); //get rid of any buttons that were there
 
+	//iterate through the tagsCount object
 	var key;
 	for(key in tagsCount)
 	{	
+		//make sure times is grammatically
 		var amount = "times";
 		if(tagsCount[key] == 1)
 		{
@@ -66,13 +84,18 @@ function addButtons()
 			amount = "time";
 		}
 
+		//add the button to the container
 		$buttonContainer.append('<div class="tagButton" onclick="highlight(\'' + key.toString() + '\')"><div class="tagButtonText">' + key + '</div> <div class="tagButtonText">' + tagsCount[key] + ' ' + amount + '</div></div>');
 	}
+	//add a filler div to make the alignment correct (hacky, I know)
 	$buttonContainer.append("<div class='tagButton empty'></div>")
 }
 
+//given a tag name, highlight the correct tags
 function highlight(tag)
 {
+	//get rid of all previous highlights
 	$("#codehere").unhighlight();
+	//highlight every varation of the tag
 	$("#codehere").highlight(["<" + tag + ">", "<" + tag, "</" + tag + ">"]);
 }
